e034b7bcbe493f5071b379f8b6cdc4b5ff398b2c
luisz <202202671@alu.comilla.edu>
2023-04-05 19:12:54 +0200

    2023.04.05:19.12

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import os
+from keras import models
+from keras import optimizers
+from keras.models import load_model
+from keras.preprocessing import image
+import json
+
+def main():
+    
+    # photos given
+    if len(sys.argv) >= 2:
+        paths = sys.argv[1:]
+
+    # reading photos from path
+    else:
+        path_photos = "photos/negative/"
+        paths = [os.path.join(path_photos, p) for p in os.listdir(path_photos)]
+    
+    model = load_model('Version1.h5')
+
+    output = []
+    for path in paths:
+        img = image.load_img(path, target_size=(300,300))
+        img_array = image.img_to_array(img)
+        img_array = img_array / 255.0
+        img_array = img_array.reshape((1,300,300,3))
+        positive_prob = model.predict(img_array)
+        output_ = {'path': path,
+                   'positive_prob': positive_prob,
+                   'positive': positive_prob > 0.5}
+        output.append(output_)
+    with open('output.json', 'w') as f:
+        json.dump(output, f)
+    print('output.json saved')
+        
+    return
+
+if __name__ == '__main__':
+    main()
